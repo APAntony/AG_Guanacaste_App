724de7458fc5145e905e59bbfd0a43b73b9d0fb0
dani0105 <danielrojas0105@gmail.com>
2022-04-24 22:08:17 -0600

    Agrega las pruebas de Auth

--- mobile_aplicacion/cypress/integration/auth-module.ts
+++ mobile_aplicacion/cypress/integration/auth-module.ts
@@ -1,14 +1,32 @@
 describe('Auth Module', () => {
-  it('login', () => {
+
+  beforeEach(() => {
     cy.intercept('/auth/login').as('login');
 
     cy.visit('/login');
-    cy.get('input[formControlName=email]').type('admin@email.com');
-    cy.get('input[formControlName=password]').type('admin@email.com');
+    cy.get('input[formControlName=email]').type('user@email.com');
+    cy.get('input[formControlName=password]').type('password');
     cy.get('ion-button[type=submit]').click();
 
     cy.wait('@login').then((interception) => {
       expect(interception.response.statusCode).eql(200);
+      cy.url().should('include', '/main-menu').end();
     });
   });
+
+  it('Update User', () => {
+    cy.intercept('/users/*').as('update');
+    cy.visit('/update-user');
+
+    cy.get('input[formControlName=name]').type('Auto Testing');
+    cy.get('input[formControlName=email]').type('user@email.com');
+    cy.get('input[formControlName=password]').type('password');
+    cy.get('ion-button[type=submit]').click();
+
+    cy.wait('@update').then((interception) => {
+      expect(interception.response.statusCode).eql(200);
+      cy.url().should('include', '/main-menu').end();
+    });
+  });
+
 });
